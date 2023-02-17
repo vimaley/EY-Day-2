f68f09cfa8e54a10c58a2d403bcee5263343206c
vimaley <Vimal.George1@gds.ey.com>
2023-02-17 05:05:13 +0000

    File 2 upload

---
+++ EY Day 2/Pgm2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace A
+{
+	class myclass1
+	{
+		public void function1()
+		{
+			Console.WriteLine(" A namespace - myclass1 -- function1()");
+		}
+	}
+}
+
+namespace B
+{
+	class myclass2
+	{
+		public void function2()
+		{
+			Console.WriteLine(" B namespace - myclass2 -- function2()");
+		}
+	}
+}
+
+namespace C
+{
+	class myclass3
+	{
+		public void function3()
+		{
+			Console.WriteLine(" C namespace - myclass3 -- function3()");
+		}
+	}
+}
+
+class india
+{
+	static void Main(string[] args)
+	{
+		A.myclass1 obj1 = new A.myclass1();
+		obj1.function1();
+		B.myclass2 obj2 = new B.myclass2();
+		obj2.function2();
+		C.myclass3 obj3 = new C.myclass3();
+		obj3.function3();
+	}
+}
+	
+
--- EY Day 2/Program.cs
+++ EY Day 2/Program.cs
@@ -1,26 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace EY_Day_2
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			char[] arr = { 'a', 'e', 'i', 'o', 'u' };
-			String s1 =  new String(arr);
-			string s2 = "Welcome to C#";
-			Console.WriteLine(s1);
-			Console.WriteLine(s2);
-			StringBuilder addword = new StringBuilder("Spiderman");
-			addword.Append(", Superman");
-			addword.Append(",Batman");
-			Console.WriteLine(addword);
+//namespace EY_Day_2
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			char[] arr = { 'a', 'e', 'i', 'o', 'u' };
+//			String s1 =  new String(arr);
+//			string s2 = "Welcome to C#";
+//			Console.WriteLine(s1);
+//			Console.WriteLine(s2);
+//			StringBuilder addword = new StringBuilder("Spiderman");
+//			addword.Append(", Superman");
+//			addword.Append(",Batman");
+//			addword.Remove(0, 9);
+//			Console.WriteLine(addword);
 
-			Console.ReadLine();
-		}
-	}
-}
+//			Console.ReadLine();
+//		}
+//	}
+//}
